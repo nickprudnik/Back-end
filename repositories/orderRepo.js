e59b5ad77dc7cb3dd61e6876ee9e3a4a37a422a4
Mikita <prudniknick@gmail.com>
2019-05-10 19:02:05 +0300

    some fixes

--- repositories/orderRepo.js
+++ repositories/orderRepo.js
@@ -2,5 +2,6 @@ const Order = require("../models/orderHistoryModel");
 
 exports.saveOrder = async ordertData => {
   const order = new Order(ordertData);
+  console.log(order);
   return await order.save();
 };
